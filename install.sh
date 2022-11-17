7f73f915a84b0fe02299b271055d69c12ac7f4f5
Michael <iamwuming@163.com>
2022-11-18 01:12:47 +0800

    use wget to download vim-plug

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 basePath=$(dirname $(readlink -f "$0"))
-echo "cwd is: $basePath"
+cd $basePath
 
 # https://github.com/junegunn/vim-plug
-if [ ! -f "${basePath}/autoload/plug.vim" ]; then
-    curl -fLo ${basePath}/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
+mkdir -p autoload && cd autoload
+[ ! -f plug.vim ] && wget --no-check-certificate \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# -h means if file is symbol link or not
+# backup old config
 [ -h ~/.vimrc ] && rm -vf ~/.vimrc
 [ -h ~/.vim ] && rm -vf ~/.vim
-
 [ -f ~/.vimrc ] && mv -v ~/.vimrc ~/.vimrc_bak
 [ -d ~/.vim ] && mv -v ~/.vim ~/.vim_bak
 
 ln -sv ${basePath} ~/.vim
 ln -sv ${basePath}/vimrc ~/.vimrc
-
